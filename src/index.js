7e2d1551da354b9120fc018a5473cd76e3d8701f
karatechops <contact@lazers.tv>
2016-10-07 15:09:56 -0400

    Updated line length.

--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ import './lib/modernizr';
 if (! Modernizr.flexbox ||
   ! Modernizr.rgba) {
   alert('Unfortunately, your browser appears to be too old. ' +
-    'We recommend the latest version of Chrome, Firefox, Safari, or Internet Explorer. ' +
-    'If you are using the latest Internet Explorer, you will need to turn off Compatibility Mode.');
+    'We recommend the latest version of Chrome, Firefox, Safari, ' +
+    'or Internet Explorer. If you are using the latest Internet Explorer, ' +
+    'you will need to turn off Compatibility Mode.');
 }
 
 import App from './js/App';
